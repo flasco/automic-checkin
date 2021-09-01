f02e2dc0ca8a2e06a9bc89fc21b0f8ae58208c7c
flasco <flasco@163.com>
2021-09-01 17:30:58 +0800

    fix: 优化api请求

--- src/api.rs
+++ src/api.rs
@@ -3,29 +3,10 @@ use std::error::Error;
 use reqwest::{self, Method};
 use serde_json::Value;
 
-pub async fn is_checkin(cookie: &str) -> Result<bool, Box<dyn Error>> {
+async fn request(method: Method, url: &str, cookie: &str) -> Result<Value, Box<dyn Error>> {
     let cli = reqwest::Client::new();
     let result = cli
-        .request(
-            Method::GET,
-            "https://api.juejin.cn/growth_api/v1/get_today_status",
-        )
-        .header("cookie", cookie)
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    let res: Value = serde_json::from_str(&result)?;
-    // println!("？？？{:#?}", res["data"]);
-
-    Ok(res["data"].as_bool().unwrap_or(false))
-}
-
-pub async fn checkin(cookie: &str) -> Result<(), Box<dyn Error>> {
-    let cli = reqwest::Client::new();
-    let result = cli
-        .request(Method::POST, "https://api.juejin.cn/growth_api/v1/check_in")
+        .request(method, url)
         .header("cookie", cookie)
         .send()
         .await?
@@ -37,6 +18,28 @@ pub async fn checkin(cookie: &str) -> Result<(), Box<dyn Error>> {
     if res["err_no"].as_i64().unwrap() != (0 as i64) {
         panic!("{}", res["err_msg"].as_str().unwrap_or("fail to get msg"))
     }
+
+    Ok(res)
+}
+
+pub async fn is_checkin(cookie: &str) -> Result<bool, Box<dyn Error>> {
+    let res = request(
+        Method::GET,
+        "https://api.juejin.cn/growth_api/v1/get_today_status",
+        cookie,
+    )
+    .await?;
+
+    Ok(res["data"].as_bool().unwrap_or(false))
+}
+
+pub async fn checkin(cookie: &str) -> Result<(), Box<dyn Error>> {
+    let res = request(
+        Method::POST,
+        "https://api.juejin.cn/growth_api/v1/check_in",
+        cookie,
+    )
+    .await?;
 
     let data = res["data"].as_object().unwrap();
     println!(
@@ -47,23 +50,12 @@ pub async fn checkin(cookie: &str) -> Result<(), Box<dyn Error>> {
 }
 
 pub async fn have_free_lottery_count(cookie: &str) -> Result<bool, Box<dyn Error>> {
-    let cli = reqwest::Client::new();
-    let result = cli
-        .request(
-            Method::GET,
-            "https://api.juejin.cn/growth_api/v1/lottery_config/get",
-        )
-        .header("cookie", cookie)
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    let res: Value = serde_json::from_str(&result)?;
-
-    if res["err_no"].as_i64().unwrap() != (0 as i64) {
-        panic!("{}", res["err_msg"].as_str().unwrap_or("fail to get msg"))
-    }
+    let res = request(
+        Method::GET,
+        "https://api.juejin.cn/growth_api/v1/lottery_config/get",
+        cookie,
+    )
+    .await?;
 
     let data = res["data"].as_object().unwrap();
     // println!("{:?}", data);
@@ -71,23 +63,12 @@ pub async fn have_free_lottery_count(cookie: &str) -> Result<bool, Box<dyn Error
 }
 
 pub async fn lottery(cookie: &str) -> Result<(), Box<dyn Error>> {
-    let cli = reqwest::Client::new();
-    let result = cli
-        .request(
-            Method::POST,
-            "https://api.juejin.cn/growth_api/v1/lottery/draw",
-        )
-        .header("cookie", cookie)
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    let res: Value = serde_json::from_str(&result)?;
-
-    if res["err_no"].as_i64().unwrap() != (0 as i64) {
-        panic!("{}", res["err_msg"].as_str().unwrap_or("fail to get msg"))
-    }
+    let res = request(
+        Method::POST,
+        "https://api.juejin.cn/growth_api/v1/lottery/draw",
+        cookie,
+    )
+    .await?;
 
     let data = res["data"].as_object().unwrap();
     println!("获得 - {}", data["lottery_name"]);
